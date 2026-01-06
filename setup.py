478162042d459d118727ff2ac7f9452db27e1c36
Deepanshu Verma <depanshuverma2000@gmail.com>
2026-01-06 11:33:12 +0530

    added cli integration

--- setup.py
+++ setup.py
@@ -8,14 +8,16 @@ setup(
     version="0.1.0",
     author="169PI",
     author_email="contact@169pi.com",
-    description="Production-ready Python SDK for the Alpie reasoning model API platform",
+    description="Production-ready Python SDK for the Pi169 reasoning model API platform",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/169Pi/Alpie-Chat-SDK",
-    package_dir={"": "src"},                 # IMPORTANT because of src/ layout
-    packages=find_packages(where="src"),     # finds src/pi169
+
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     include_package_data=True,
     license="MIT",
+
     install_requires=[
         "certifi>=2025.11.12",
         "httpx>=0.28.1",
@@ -26,13 +28,16 @@ setup(
         "packaging>=25.0",
         "python-dotenv",
     ],
+
     extras_require={
         "dev": [
             "pytest>=9.0.1",
             "pytest-mock>=3.15.1",
         ]
     },
+
     python_requires=">=3.10",
+
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
@@ -41,4 +46,11 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
     ],
+
+    # CLI entry point
+    entry_points={
+        "console_scripts": [
+            "pi169=pi169.cli:main",
+        ]
+    },
 )
